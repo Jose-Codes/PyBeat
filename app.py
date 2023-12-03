f7465706a5a5ab91c1408194c9085620979b681c
Jose Pujol <josepujol21@gmail.com>
2023-12-02 20:21:09 -0500

    resolved merge conflicts.

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
 import streamlit as st
 import requests
-from views import MapView
+import pandas as pd
 
+#views
+from views import events_by_country
+from views import events_per_month
+from views import table_view
+from views import map_view
 
-category = st.sidebar.selectbox("Choose event details", options=["Map", 
-"Table", "View available events by date", "View available artist events by country", "View artist events per month"])
-
-if category == "Map":
-    # TODO: Make Map
-    st.title("Map of events")
-    st.markdown("This is a map of events in the world")
-    MapView.map_view()
-elif category == "Table":
-    # TODO: Make Table
-    st.title("Table of events")
-    st.markdown("This is a table of events in the world")
-    st.table()
-elif category == "View available events by date":
-    # TODO: Make available events by date Table
-    st.title("View available events by date")
-    st.markdown("This is a table of events in the world")
-    st.table()
-elif category == "View available artist events by country":
-    # TODO: Make available artist events by country Bar Chart
-    st.title("View available artist events by country")
-    st.markdown("This is a table of events in the world")
-    st.table()
-elif category == "View artist events per month":
-    # TODO: Make artist events per month line chart
-    st.title("View artist events per month")
-    st.markdown("This is a table of events in the world")
-    st.table()
+with st.sidebar:
+    artist = st.text_input("Search an Artist")
+    category = st.selectbox("Choose event details", options=["Map", "Table", "View available events by date", "View available artist events by country", "View artist events per month"])
+if(artist):
+    if category == "Map":
+        # TODO: Make Map
+        st.title("Map of events")
+        st.markdown("This is a map of events in the world")
+        map_view.map_view(artist)
+    elif category == "Table":
+        # TODO: Make Table
+        table_view.display_table(artist)
+    elif category == "View available events by date":
+        # TODO: Make available events by date Table
+        st.title("View available events by date")
+        st.markdown("This is a table of events in the world")
+        st.table()
+    elif category == "View available artist events by country":
+        # TODO: Make available artist events by country Bar Chart
+        events_by_country.get_artists_events_by_country(artist)
+    elif category == "View artist events per month":
+        # TODO: Make artist events per month line chart
+        events_per_month.get_artists_event_per_month(artist)
+    else:
+        # Default page view
+        st.title("Map of events")
+        st.markdown("This is a map of events in the world")
+        st.map()
 else:
-    # Default page view
-    st.title("Map of events")
-    st.markdown("This is a map of events in the world")
-    st.map()
+        st.warning("Please enter an artist name.", icon="⚠️")
